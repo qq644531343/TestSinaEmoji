1fde858851d1ab64b100e4df2f71bad14774cdea
libo_liu <qq644531343@gmail.com>
2014-08-26 13:07:37 +0800

    add sth

---
+++ TestSinaEmoji/TestSinaEmoji/VDEmoji/VDEmojiToolLabel.h
@@ -0,0 +1,13 @@
+//
+//  VDEmojiToolLabel.h
+//  TestSinaEmoji
+//
+//  Created by libo on 8/25/14.
+//  Copyright (c) 2014 sina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VDEmojiToolLabel : UILabel
+
+@end
---
+++ TestSinaEmoji/TestSinaEmoji/VDEmoji/VDEmojiToolLabel.m
@@ -0,0 +1,31 @@
+//
+//  VDEmojiToolLabel.m
+//  TestSinaEmoji
+//
+//  Created by libo on 8/25/14.
+//  Copyright (c) 2014 sina. All rights reserved.
+//
+
+#import "VDEmojiToolLabel.h"
+
+@implementation VDEmojiToolLabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
